73398cfe62fe7228a18fca0dfd03a990a30edf1f
Anton Evangelatov <anton.evangelatov@gmail.com>
2019-03-23 22:25:55 +0100

    promote smoke test logs to Debug

--- cmd/swarm/swarm-smoke/upload_and_sync.go
+++ cmd/swarm/swarm-smoke/upload_and_sync.go
@@ -80,7 +80,7 @@ func trackChunks(testData []byte) error {
 	}
 
 	for i, ref := range addrs {
-		log.Trace(fmt.Sprintf("ref %d", i), "ref", ref)
+		log.Debug(fmt.Sprintf("ref %d", i), "ref", ref)
 	}
 
 	for _, host := range hosts {
@@ -115,7 +115,7 @@ func trackChunks(testData []byte) error {
 			log.Info("host reported to have all chunks", "host", host)
 		}
 
-		log.Trace("chunks", "chunks", strings.Join(hostChunks, ""), "host", host)
+		log.Debug("chunks", "chunks", strings.Join(hostChunks, ""), "host", host)
 	}
 	return nil
 }
